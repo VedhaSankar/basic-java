c67eaedc73c473d3542aede3e1f60fa99ee430d3
VedhaSankar <vedhasankar26@gmail.com>
2021-08-10 23:51:25 +0530

    sample program for input and output

--- sample.java
+++ sample.java
@@ -9,16 +9,17 @@ public class test {
         Scanner scan = new Scanner(System.in);
 
 
-
         System.out.println("Enter name:");
         String name = scan.nextLine();
 
         System.out.println("Enter age:");
-        int age = scan.nextInt();
+        int age = Integer.parseInt(scan.nextLine());
+
+        // it doesn't input address here if we use nextInt. https://stackoverflow.com/questions/13102045/scanner-is-skipping-nextline-after-using-next-or-nextfoo
 
         System.out.println("Enter address:");
         String address = scan.nextLine();
-
+        
         System.out.println(name + " " + age + " " + address);
     }
 
