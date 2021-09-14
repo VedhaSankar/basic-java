6342a9bffc3b47ca589ea2bc224397b4c52d36f6
VedhaSankar <vedhasankar26@gmail.com>
2021-09-15 02:06:12 +0530

    packages implemented

---
+++ assignmentPackage/myPackage.java
@@ -0,0 +1,17 @@
+package assignmentPackage;
+
+public class myPackage {
+
+    public void stuff(){
+
+        System.out.println("Hello from the other side");
+        
+    }
+
+    public static void main(String args[]){
+
+        System.out.println("Hello from the other side");
+
+    }
+    
+}
---
+++ myAssignment.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import  assignmentPackage.*;
+
+
+public class myAssignment {
+
+    public static void main(String args[]){
+
+        myPackage obj = new myPackage();
+        obj.stuff();
+
+    }
+    
+}
