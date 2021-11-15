ebecaec9af662f52ccd62f2be4ed1627a2685b46
VedhaSankar <vedhasankar26@gmail.com>
2021-11-16 03:11:25 +0530

    array list

---
+++ array_list.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class array_list {
+    public static void main(String[] args) {
+
+
+        ArrayList<String> fruits = new ArrayList<String>();
+
+        fruits.add("Pineapple");
+        fruits.add("Orange");
+
+        System.out.println(fruits);
+        
+    }
+}
